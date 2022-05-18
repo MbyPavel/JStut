fa846efbca26201a1583fc6f479120b470e91caa
Pavel <pavel.strakos423@seznam.cz>
2022-05-18 10:23:53 +0200

    ZakladyJS

---
+++ JStutorial/index.js
@@ -0,0 +1,66 @@
+// Coment
+// console.log('Hello World');
+
+// let name = 'John';
+// console.log(name);
+
+// Cannot be a reserved word
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or a hyphen (-name)
+
+// let firstName = 'John'
+// let lastName = 'Doe'
+// let FirstName;
+
+// let intrestRate = 0.3;
+// intrestRate = 1;
+// console.log(intrestRate);
+
+/*
+let name = 'John'; // string
+let age = 30; // number
+let isApproved = false; // boolean
+let firstName = null; // object
+let lastName = undefined; // undefined
+let selectedColor = null; 
+*/
+
+/*
+let person = {
+    firstName: 'John',
+    age: 30
+};
+
+// Dot notation
+person.name = 'Mary';
+
+// Bracket notation
+let selection = 'name';
+person[selection] = 'Mosh';
+
+console.log(person);
+*/
+
+/*
+let selectedColors = ['red', 'green', 'blue'];
+selectedColors[2] = 1;
+console.log(selectedColors.length);
+*/
+
+/*
+// Performing a task
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+// Calculating a value
+function square(number) {
+    return number * number;
+}
+
+console.log(square(3));
+
+greet('John', 'Doe');
+greet('Mary', 'Jane');
+*/
